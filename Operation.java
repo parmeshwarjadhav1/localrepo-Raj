bc194cec89fbefd07a5a10fd9030533d8d129b77
Umesh Rohidas Jadhav <umeshjadhavxig@gmail.com>
2024-07-18 16:36:30 +0530

    New Add Java File

---
+++ Operation.java
@@ -0,0 +1,7 @@
+public class Operation {
+    public static void main(String arg[]) {
+        int a = 10;
+        int b = 40;
+        System.out.println("Addition Of Two Numbers :- " + (a + b));
+    }
+}
